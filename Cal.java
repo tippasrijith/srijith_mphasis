74c1bf0a42cdeef1a99553d66fa4ee9e91dd83d7
tippasrijith <39362667+tippasrijith@users.noreply.github.com>
2018-05-17 11:14:31 +0530

    Add files via upload

---
+++ Cal.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Cal
+{
+ public static void main(String args[])
+{
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter emp no");
+ int a=sc.nextInt();
+ String name=sc.next();
+ float salary=sc.nextFloat();
+
+System.out.println("the empno is"+a+"name is"+name+"salary is"+salary);
+}
+}
